9f21b2683ec150f7c15e7be8fbf6f07bc3e593f6
Rafa.mattss <rain.matt@gmail.com>
2024-12-15 23:10:56 -0300

    feat: integration

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rest_framework import serializers
 from .models import Machine, Maintenance, Team, Part, UsedPart, Profile
 from django.contrib.auth.models import User
@@ -18,10 +19,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
-class MachineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Machine
-        fields = '__all__'
+
+
 
 class TeamSerializer(serializers.ModelSerializer):
     # Usando PrimaryKeyRelatedField para aceitar apenas o id do líder e dos membros
@@ -33,15 +32,24 @@ class TeamSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'leader', 'members']
 
 class MaintenanceSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-    machine = serializers.SerializerMethodField()
-    team = serializers.SerializerMethodField()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    machine = serializers.PrimaryKeyRelatedField(queryset=Machine.objects.all())
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all())  # Modificação aqui
     formatted_date = serializers.SerializerMethodField()
 
     class Meta:
         model = Maintenance
         fields = ['id', 'machine', 'team', 'user', 'formatted_date', 'date', 'status', 'priority', 'description']
 
+    def validate_date(self, value):
+        if isinstance(value, datetime):  # Verifica se o valor já é um datetime
+            return value
+        try:
+            return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")  # Formato ISO 8601
+        except ValueError:
+            raise serializers.ValidationError("Formato de data inválido. Use o formato ISO 8601.")
+
+    
     def get_user(self, obj):
         return f"{obj.user.username}" if obj.user else None
 
@@ -61,14 +69,20 @@ class MaintenanceSerializer(serializers.ModelSerializer):
         }
 
     def get_formatted_date(self, obj):
-        # Formata a data no formato desejado
         return localtime(obj.date).strftime('%d/%m/%Y %H:%M') if obj.date else "N/A"
+
 
 class PartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Part
         fields = '__all__'
 
+class MachineSerializer(serializers.ModelSerializer):
+    history = MaintenanceSerializer(many=True)  # Adiciona a lista de manutenções associadas à máquina
+
+    class Meta:
+        model = Machine
+        fields = '__all__'
 
 class UsedPartSerializer(serializers.ModelSerializer):
     part = PartSerializer()
